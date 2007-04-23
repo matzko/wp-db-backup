8a5fae2a962d90794a7b2dc5c2fe82d274c502c4
matzko <austin@pressedcode.com>
2007-04-23 03:22:20 +0000

    correct version

--- wp-db-backup.php
+++ wp-db-backup.php
@@ -5,7 +5,7 @@ Plugin URI: http://www.ilfilosofo.com/blog/wp-db-backup
 Description: On-demand backup of your WordPress database. Navigate to <a href="edit.php?page=wp-db-backup.php">Manage &rarr; Backup</a> to get started.
 Author: Austin Matzko 
 Author URI: http://www.ilfilosofo.com/blog/
-Version: 2.0.3
+Version: 2.0.4
 
 Development continued from that done by Skippy (http://www.skippy.net/)
 
